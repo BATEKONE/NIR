8e71b98729ec90edb6ec6b4d4c10bc388e7bb988
BATEKONE <svytoslav2002medvedev@gmail.com>
2025-05-27 14:16:58 +0300

    commit for downloaded dataset

---
+++ downloadDataset.py
@@ -0,0 +1,5 @@
+import kagglehub
+path = kagglehub.dataset_download("hozngvan/helmet-detection", force_download=True)
+print("Path to dataset files:", path)
+
+# C:\Users\svyto\.cache\kagglehub\datasets\hozngvan\helmet-detection\versions\11
